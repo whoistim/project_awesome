dce8706ebd6134cd76486fab958b4475dda12293
prschulz <prschulz88@gmail.com>
2014-11-19 15:14:05 -0800

    added home controller with index, about, and FAQ

--- Rspots/config/routes.rb
+++ Rspots/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/about'
+
+  get 'home/faq'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
