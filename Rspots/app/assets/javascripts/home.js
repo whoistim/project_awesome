b2f608fa55d186757991ff0977d019c1ae2789ac
Melody Serra <melodyserra@gmail.com>
2014-11-20 15:23:08 -0800

    google places api integrated, but had to run rake db:creat and migrate without models being set up yet

---
+++ Rspots/app/assets/javascripts/home.js
@@ -0,0 +1,20 @@
+google.maps.event.addDomListener(window, 'load', function () {
+	console.log("Hello");
+	var map = new google.maps.Map(document.getElementById('map-canvas'), {
+		zoom: 17,
+		center: new google.maps.LatLng(37.7912563,-122.4006792),
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		zoomControl: false,
+		scrollwheel: false,
+  });
+
+var input = ( document.getElementById('pac-input')); 
+
+map.controls[google.maps.ControlPosition.TOP_RIGHT].push(input);
+
+
+// var defaultBounds = new google.maps.LatLngBounds(
+  //     new google.maps.LatLng(37.79086,-122.40147), 
+  //     new google.maps.LatLng(37.8337, -122.3143));
+  // map.fitBounds(defaultBounds);
+});
