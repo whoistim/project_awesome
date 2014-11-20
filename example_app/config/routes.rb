51fa7c3e6574fc9a95a0df91c587d7b4cbbeefe1
prschulz <prschulz88@gmail.com>
2014-11-19 18:35:18 -0800

    adding home controller and root route to home

--- example_app/config/routes.rb
+++ example_app/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+
+  root 'welcome#index'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
