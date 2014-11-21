5422b1c63be9b1a5bae66623bd3d7aae6b909870
Melody Serra <melodyserra@gmail.com>
2014-11-21 10:07:16 -0800

    can now search places

--- Rspots/app/assets/javascripts/home.js
+++ Rspots/app/assets/javascripts/home.js
@@ -15,9 +15,52 @@ var input = ( document.getElementById('pac-input'));
 
 map.controls[google.maps.ControlPosition.TOP_RIGHT].push(input);
 
-// var defaultBounds = new google.maps.LatLngBounds(
-  //     new google.maps.LatLng(37.79086,-122.40147), 
-  //     new google.maps.LatLng(37.8337, -122.3143));
-  // map.fitBounds(defaultBounds);
+//Code added to be able to search for a place and then mark it on the map:
+
+var searchBox = new google.maps.places.SearchBox((input));
+  google.maps.event.addListener(searchBox, 'places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    var markers = [];
+
+    if (places.length == 0) {
+      return;
+    }
+    for (var i = 0, marker; marker = markers[i]; i++) {
+      marker.setMap(null);
+    }
+
+// For each place, get the icon, place name, and location.
+markers = [];
+var bounds = new google.maps.LatLngBounds();
+for (var i = 0, place; place = places[i]; i++) {
+  var image = {
+    url: place.icon,
+    size: new google.maps.Size(71, 71),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(17, 34),
+    scaledSize: new google.maps.Size(25, 25)
+  };
+
+// Create a marker for each place.
+var marker = new google.maps.Marker({
+  map: map,
+  icon: image,
+  title: place.name,
+  position: place.geometry.location
+});
+
+markers.push(marker);
+
+bounds.extend(place.geometry.location);
+}
+
+map.fitBounds(bounds);
+});
+
+google.maps.event.addListener(map, 'bounds_changed', function() {
+	var bounds = map.getBounds();
+	searchBox.setBounds(bounds);
+	});
 
 });
