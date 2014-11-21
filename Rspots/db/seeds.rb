2a42e2fc327b1a68caaf7490e82b21580d0a9d22
prschulz <prschulz88@gmail.com>
2014-11-21 11:55:06 -0800

    adding seed data ... not finished yet

--- Rspots/db/seeds.rb
+++ Rspots/db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Delete all previous seed data // blank slate
+User.delete_all
+Group.delete_all
+Review.delete_all
+Location.delete_all
+GroupLocation.delete_all
+GroupUser.delete_all
+
+# Users
+u1 = User.create(provider: "facebook", uid: "10203844294482291", first_name: "Peter", last_name: "Schulz", image: "http://graph.facebook.com/10203844294482291/picture")
+u2 = User.create(provider: "facebook", uid: "10203844294482291", first_name: "Melody", last_name: "Serra", image: "http://graph.facebook.com/10203844294482291/picture")
+u3 = User.create(provider: "facebook", uid: "10203844294482291", first_name: "Tim", last_name: "Gray", image: "http://graph.facebook.com/10203844294482291/picture")
+
+#Group
+g1 = Group.create(name: "General Assembly", lat:"37.7912563", lng:"-122.4006792",)
