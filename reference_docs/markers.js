88a0b8fdcafae65ee2976431ea9f301a8139b2f3
whoistim <whoistim@sbcglobal.net>
2014-11-21 14:57:18 -0800

    synching up with upstream

--- reference_docs/markers.js
+++ reference_docs/markers.js
@@ -1,10 +1,24 @@
 //get locations from gmap into an array
-var places = {};
-places = $.getJSON("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.7912563,-122.4006792&radius=500&types=restaurant&key=AIzaSyDo_Bxtq4HV1n84J8yd-L7G47vzMAyksPg", function(result){
-
-	console.log(result);
+//get locations into an array
+var arr_places = {};
+$.getJSON("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.7912563,-122.4006792&radius=500&types=restaurant&key=AIzaSyDo_Bxtq4HV1n84J8yd-L7G47vzMAyksPg", function(result){
+	result = arr_places;
+	// console.log(result);
 });
 
+for (var i = 0, place; place = arr_places[i]; i++) {
+  if (place.reviewed){
+  	var image = {url: assets/images/icon1.png};
+	  }
+  else{
+  	var image = {
+    url: assets/images/icon2.png};
+	  }
+var marker = new google.maps.Marker({
+    position: place.,
+    map: map,
+
+	}
 
 
 
@@ -15,3 +29,4 @@ places = $.getJSON("https://maps.googleapis.com/maps/api/place/nearbysearch/json
 //put markers onto the map
 
 
+//recenter the map on the new location
