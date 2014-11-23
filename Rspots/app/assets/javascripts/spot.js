ea0479011faa6881fc4ab99bb8df851d8cdf45ad
prschulz <prschulz88@gmail.com>
2014-11-23 12:29:24 -0800

    changed name of variable for logo so diff from marker image used later

--- Rspots/app/assets/javascripts/spot.js
+++ Rspots/app/assets/javascripts/spot.js
@@ -2,25 +2,26 @@
 // var grpLatlng = new google.maps.LatLng(my_group.lat,my_group.lng);
 var myLatlng = new google.maps.LatLng(37.7908767,-122.4016454);
 
-//Places the GA marker on the map
-var myMarker = function(myLatlng,map){
-	new google.maps.Marker({
-    position: myLatlng,
-    map: map,
-    icon: image,
-    title:"General Assembly"
-  });
-};
-
-
-// sets image as GA logo -- NEED TO CHANGE TO GET LOGO FROM DB in v2
-var image = {
+// sets logo as GA logo -- NEED TO CHANGE TO GET LOGO FROM DB in v2
+var logo = {
   url: '/assets/GA.png',
   size: new google.maps.Size(71, 71),
   origin: new google.maps.Point(0, 0),
   anchor: new google.maps.Point(17, 34),
   scaledSize: new google.maps.Size(40, 40)
 };
+
+//Places the GA marker on the map
+var myMarker = function(myLatlng,map){
+	new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    icon: logo,
+    title:"General Assembly"
+  });
+};
+
+
 var my_group;
 
 var our_locations  = [{id: 1, title: "Mixt Greens", lat: "-122.40063700000002", lng: "37.791578", address: "120 Sansome St., San Francisco, CA", place_id: "ChIJ62yCEWKAhYARNL0YQVpRuKg", website: "http://mixtgreens.com", phone_number: "415-555-5555", created_at: "2014-11-21 23:09:45", updated_at: "2014-11-21 23:09:45"},
