ca314da41114fbd438f84bdadadf3ab4e7eb7ee1
prschulz <prschulz88@gmail.com>
2014-11-24 20:55:00 -0800

    Example app removed

--- example_app/app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  devise :omniauthable,
-         :omniauth_providers => [:facebook]
-end
